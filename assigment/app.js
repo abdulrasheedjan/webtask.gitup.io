48240163c4096a498fa953acfb15adabdd7286ad
jano ka jan <158552934+abdulrasheedjan@users.noreply.github.com>
2024-05-19 20:26:32 +0500

    Add files via upload

---
+++ assigment/app.js
@@ -0,0 +1,19 @@
+var textarea1 = document.getElementById("textarea1")
+var bold1 = document.getElementById("bold1")
+var italic1 = document.getElementById("italic1")
+var underline1 = document.getElementById9("underline1")
+color1.innerHTML=color1
+
+function boldfun(){
+    textarea1.style.fontWeight = 'bold';
+    if(style==="bold"){
+        style=normal
+    }
+}
+
+function italicfun(){
+    textarea1.style.fontStyle = 'italic'
+}
+function underlinefun(){
+    underline1.style.textdecoration = 'underline'
+}
